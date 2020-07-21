dd600f46d65db27b82ca1bbfcbfa7534c58e99c2
axel_j <axist8@gmail.com>
2020-07-21 15:12:16 -0700

    2.2

--- lessons.js
+++ lessons.js
@@ -4,7 +4,19 @@ for (let i = 0 ; i < 7 ; i++){
   console.log(mark);
   mark = mark + "#";
 }
-// 2.2 
+// 2.2 FizzBuzz
+for (let i = 1 ; i < 101 ; i++){
+    if (i % 15 == 0){
+      console.log("FizzBuzz");
+    } else if (i % 5 == 0){
+      console.log("Buzz");
+    } else if (i % 3 == 0){
+      console.log("Fizz");
+    } else {console.log(i);}
+  }
+
+// 2.3 Chessboard
+
 
 // 4.1 Sum of a range
 function range(start, end, step){
